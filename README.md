c26835e8bc4393b3de552380b47d9af9dbe6b9c9
xu0329 <2822738897@qq.com>
2023-12-09 17:44:58 +0800

    代理池刷播放量

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## 通过代理池刷播放量
+
+本项目只适用于通过代理池刷取  
+效率高，速度快  
+
+ 
+**使用**：只需修改以下部分即可  
+![image](image/image.jpeg)
+
+**[Ven3/playcount](https://github.com/Ven3/playcount)** 包含不使用代理池功能
+
+
+### 代理池
+[jhao104/proxy_pool](https://github.com/jhao104/proxy_pool)
---
+++ bilibili_proxy.py
@@ -0,0 +1,81 @@
+import requests
+import time
+
+
+#视频bv号放这里，格式如下
+
+bvid=["xxxx","xxxx"]
+
+
+#代理池地址，项目可见https://github.com/jhao104/proxy_pool
+
+getproxy = "http://127.0.0.1:5010/get/"
+deleteproxy = "http://127.0.0.1:5010/delete/?proxy={}"
+proxynum="http://127.0.0.1:5010/count"
+
+
+
+url = "http://api.bilibili.com/x/click-interface/click/web/h5"
+
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Origin': 'https://www.bilibili.com',
+    'Connection': 'keep-alive'
+}
+
+reqdata = []
+for bv in bvid:
+    stime = str(int(time.time()))
+    
+    resp = requests.get("https://api.bilibili.com/x/web-interface/view?bvid={}".format(bv))
+    getdata = resp.json()["data"]
+    data= {
+        'aid':getdata["aid"],
+        'cid':getdata["cid"],
+        "bvid": bv,
+        'part':'1',
+        'mid':getdata["owner"]["mid"],
+        'lv':'6',
+        "stime" :stime,
+        'jsonp':'jsonp',
+        'type':'3',
+        'sub_type':'0',
+        'title': getdata["title"]
+    }
+    reqdata.append(data)
+
+
+
+def run():
+    num=0
+    while int(requests.get(proxynum).json().get("http_type", {}).get("http")) != 0:
+
+        resp=requests.get(getproxy).json().get("proxy")
+        for data in reqdata:
+            
+            try:
+                stime = str(int(time.time()))
+                data["stime"] = stime
+                headers["referer"] = "http://www.bilibili.com/video/{}/".format(data.get("bvid"))
+
+                proxy = {
+                    "http": "http://{}".format(resp)
+                }
+
+                requests.post(url,headers=headers,data=data,proxies=proxy,timeout=5)
+
+            except Exception as e:
+                print("代理连接超时")
+
+        requests.get(deleteproxy.format(resp))
+        num+=1
+        print("done   {}".format(num))
+
+    print("无可用代理")
+
+run()
+        
